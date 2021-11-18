433e8206fc0a36ae070b3f23f57cee558d42c36e
Anshu Naagar <83154441+AnshuNaagar@users.noreply.github.com>
2021-11-18 16:19:55 +0530

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,13 @@
+console.log("Hello,To developer")
+console.log("How Are you")
+console.table("hello", "hey")
+
+function hello() {
+    alert("We Are just working on our Read more Page ");
+}
+
+
+
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+    trigger: 'focus'
+})
